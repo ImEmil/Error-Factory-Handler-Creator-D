aeb9b9c2ab7fc1b07d4eaf7764c050b1c701b6ca
Emil Nilsson <emil_nilsson_96@live.se>
2016-04-17 02:12:11 +0200

    Update index.php

--- index.php
+++ index.php
@@ -10,7 +10,7 @@ $errorFactory = new ErrorFactory;
 $errorFactory->createCustomInterface(function($templateBuilder) use ($errorFactory)
 {
 	$html = '<div class="{error.class}" style="{error.style}">{error.message}</div>';
-
+	
 	$templateBuilder->class =
 	[
 		"error"   => ".error red message",
@@ -21,10 +21,10 @@ $errorFactory->createCustomInterface(function($templateBuilder) use ($errorFacto
 
 	$templateBuilder->style = 
 	[
-		"all" => "height:100px;width:100%;font-weight:bolder;",
-		"error" => "font-style:italic;",
+		"all"     => "height:100px;width:100%;font-weight:bolder;",
+		"error"   => "font-style:italic;",
 		"warning" => "font-weight:300;",
-		"info" => "font-size:9px;",
+		"info"    => "font-size:9px;",
 		"success" => "background:green;"
 	];
 
@@ -39,6 +39,8 @@ $messages->warning("WARNING M8");
 
 $messages
 ->info("INFO M8")
-->success("SUCCESS M8");
+->success("SUCCESS M8")
+->info("INFO M8")
+->success("testing");
 
 echo $messages->_catchMessages();
